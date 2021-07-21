4f5932b82b7e38a40d0912d7d1e87e3bf497aaa8
Zvat Scorobogatov <zvat.333333@gmail.com>
2021-07-21 20:28:24 +0300

    ls_js

---
+++ ls14/main.js
@@ -0,0 +1,29 @@
+let name = "George"; 
+let age = 18;
+let married = false; 
+/*
+// числа 1, 2.1, -16327865178
+// строки 
+// булианы (0. 1 или правда ложь)
+console.log("Привет, " + name + ". Твой возраст " + age);
+//console.log(married)
+
+console.log(1 + 1)
+console.log("1 + 1")*/
+/*
+let x, y;
+x = parseInt(prompt());
+y = parseInt(prompt());
+console.log("СУММА: " + x + y);
+console.log(x - y);
+console.log("*: " + x * y);
+console.log("/: " + x / y);
+console.log("Остаток от деления: " + x % y);
+console.log(Math.pow(x, y));
+console.log(Math.sqrt(x, y));
+
+document.write("<p>Hello " + name +"</p>");
+*/
+
+alert("АП")
+
---
+++ ls15/main.js
@@ -0,0 +1,30 @@
+//типы данных // значения
+const z = 10;   //числа 
+let x = "asd";  //строки
+let c = true;  //булианы
+let v = [];     //массивы
+let b = {};     //дерево
+a = 10;
+/*
+    операторы, 
+    и 
+    операции 
+    значения
+*/
+/*let n = class*/
+false || true // или (всегда ищет true)
+false && true // и (всегда требует true)
+false == true // равняется ли 
+false === true // равняется ли значение по типу
+false > true
+false < true
+false <= true
+false >= true
+!true 
+
+// if( true or false ) / while( true orr false ) / ( true / false )
+
+if(true)
+{
+    console.log(true)
+}
