7d0522dc75bebc4108e701a2e209eb18e32a7ba4
Zvat Scorobogatov <zvat.333333@gmail.com>
2021-08-11 20:21:26 +0300

    ls21 - 22

--- ls22/main.js
+++ ls22/main.js
@@ -1,39 +1,75 @@
-let block = document.querySelector(".a1"); 
-let user = "Jack";
-let age = 18;
+// DOM - 
 
-block.style.backgroundColor = "red";
+// mousemove - движение мышки
+// click 
+// 
 
-block.innerHTML = `
-<h1 class="h1">Hello, ${user}</h1>
-<p>lorem lorem lorem loremlorem lorem lorem</p>
-`
-console.log(block);
+const btn1 = document.querySelector('#b1');
+const btn2 = document.querySelector('#b2');
+const btn3 = document.querySelector('#b3');
+const btn4 = document.querySelector('#b4');
 
-if(age < 18){
-    block.style.display = "none";
-}else{
+const sizeX = window.innerWidth; 
 
-}
+let x = 1;
 
-console.log(document.body)
-
-let input = document.querySelector('#name');
-let button = document.querySelector('#send_messange');
+const perc = document.querySelector('.persone');
 
 
-function Hello(target){
-    console.log("Hello")
-}
-
-input.addEventListener("change", function(e){
-    console.log(input.value)
+document.addEventListener("mousedown", e => {
+    console.log("down")
 })
 
-button.addEventListener("click", function(e){
-    console.log("button")
-});
+document.addEventListener("mouseup", e => {
+    console.log("up")
+})
 
+document.addEventListener("keypress", e => {
+    switch(e.which){
+        // a
+        case 65:
+        case 97:
+        case 37:
+            if(x > 0){
+                x -= 1;
+                perc.style.left = x + "px"
+            }else{
+                console.log("Границі")
+            } 
+            break;
+        // d
+        case 68:
+        case 100:
+        case 39:
+            if(x < sizeX){    
+                x += 1;
+                perc.style.left = x + "px" 
+            
+            }
+            break;
+        default:
+            console.log(e.which)
+    }
 
+})
 
+document.addEventListener("click", (e) => {
+    switch(e.target){
+        case btn1:
+            console.log(1);
+            break;
+        case btn2:
+            console.log(2);
+            break;
+        case btn3:
+            console.log(3);
+            break;
+        case btn4:
+            console.log(4);
+            break;
+        default: 
+            console.log(NaN)
+            break;
+    }
+})
 
