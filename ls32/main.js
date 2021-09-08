5c4f306c45380619db1d07850110fb4637273036
Скоробогатов Святослав <45883075+CarbonSkorobogatov@users.noreply.github.com>
2021-09-08 21:00:49 +0300

    ls_31

---
+++ ls32/main.js
@@ -0,0 +1,47 @@
+class Geometry{
+    static Pi = 3.14;
+    static Exp = 2.7;
+
+
+    constructor(){
+
+    }
+
+
+}
+
+
+
+class Rigth_Triangle extends Geometry{
+    
+    #find_angle(){
+        if(this.A > this.B && this.A > this.C){
+            return this.#sqrt_r(this.B, this.C)
+        }else if(this.A < this.B && this.B > this.C){
+            return this.#sqrt_r(this.A, this.C)
+        }else if(this.C > this.B && this.C > this.A){
+            return this.#sqrt_r(this.B, this.A)
+        }
+    }
+
+    #sqrt_r(num1, num2){
+        return (num1*num2)/2
+    }
+
+    RigthTriangleSqrt(){
+       return this.#find_angle()
+    }
+
+    constructor(a, b, c){
+        super()
+        this.A = a;
+        this.B = b;
+        this.C = c;
+    }
+
+
+
+}
+
+ABC = new Rigth_Triangle(3, 4, 5)
+
