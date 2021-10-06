01397a09b3ae4b5a83d96abe5032aca9f8b63a96
Скоробогатов Святослав <45883075+CarbonSkorobogatov@users.noreply.github.com>
2021-10-06 20:59:17 +0300

    ls react

---
+++ react1/react1/src/App.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+import './App.css';
+
+export default function App(){
+    let name = "Sviatoslav";
+    return (
+      <div className="app">
+        <h2>Hello, {name}</h2>
+      </div>
+    );
+}
